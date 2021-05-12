c3c7bd40f581286ceace3c41fa4aad2502a8e94b
HANSAOUI_NACEUREDDINE@emsi-edu.ma <HANSAOUI_NACEUREDDINE@emsi-edu.ma>
2021-05-12 17:02:57 +0000

    Ajout De La Class Employee

---
+++ src/main/java/ma/emsiedu/employeemanager/models/Employee.java
@@ -0,0 +1,24 @@
+package ma.emsiedu.employeemanager.models;
+
+import lombok.*;
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+@Entity
+@Data
+@AllArgsConstructor @NoArgsConstructor @ToString
+public class Employee implements Serializable {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phoneNumber;
+
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+
+}
