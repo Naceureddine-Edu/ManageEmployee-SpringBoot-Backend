5a4089ec16273f51e13fccdffae982c1ae304ee9
HANSAOUI_NACEUREDDINE@emsi-edu.ma <HANSAOUI_NACEUREDDINE@emsi-edu.ma>
2021-05-12 16:55:19 +0000

    Creation Du Projet

---
+++ src/test/java/ma/emsiedu/employeemanager/EmployeemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsiedu.employeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
